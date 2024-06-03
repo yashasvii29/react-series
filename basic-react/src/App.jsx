7f3e23ff0570bbedc3bd4dd46af8a1d9de194a53
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2024-06-04 00:14:14 +0530

    add folder

---
+++ basic-react/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// ab react ko import krne ki need nhi hai because babel compiler directly react ko import kr deta hai
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
